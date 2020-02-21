ae721c937e95d858d52ac899e5ba9e611e02ecb9
Susanne Kilian <s.kilian@hhpberlin.de>
2020-02-21 10:53:29 +0100

    add Lunarc transfer scripts

---
+++ Verification/Copy_Lunarc_csv.sh
@@ -0,0 +1,19 @@
+cd Pressure_Solver
+cp *.csv Lunarc_csv
+
+cd ../Pressure_Effects
+cp *.csv Lunarc_csv
+
+cd ../Scalar_Analytical_Solution
+cp *.csv Lunarc_csv
+
+cd ../NS_Analytical_Solution
+cp *.csv Lunarc_csv
+
+cd ../Turbulence
+cp *.csv Lunarc_csv
+
+cd ../HVAC
+cp *.csv Lunarc_csv
+
+
---
+++ Verification/Move_Lunarc_csv.sh
@@ -0,0 +1,19 @@
+cd Pressure_Solver
+cp Lunarc_csv/*.csv .
+
+cd ../Pressure_Effects
+cp Lunarc_csv/*.csv .
+
+cd ../Scalar_Analytical_Solution
+cp Lunarc_csv/*.csv .
+
+cd ../NS_Analytical_Solution
+cp Lunarc_csv/*.csv .
+
+cd ../Turbulence
+cp Lunarc_csv/*.csv .
+
+cd ../HVAC
+cp Lunarc_csv/*.csv .
+
+
