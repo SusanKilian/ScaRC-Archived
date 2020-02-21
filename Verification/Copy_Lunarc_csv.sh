26d31fb455ffd7739683a53e54e3b3e7d9d93e14
Susanne Kilian MacPro <s.kilian@hhpberlin.de>
2020-02-21 11:29:17 +0100

    rework Lunarc scripts

--- Verification/Copy_Lunarc_csv.sh
+++ Verification/Copy_Lunarc_csv.sh
@@ -1,19 +1,25 @@
 cd Pressure_Solver
 cp *.csv Lunarc_csv
+cp *.txt Lunarc_csv
 
 cd ../Pressure_Effects
 cp *.csv Lunarc_csv
+cp *.txt Lunarc_csv
 
 cd ../Scalar_Analytical_Solution
 cp *.csv Lunarc_csv
+cp *.txt Lunarc_csv
 
 cd ../NS_Analytical_Solution
 cp *.csv Lunarc_csv
+cp *.txt Lunarc_csv
 
 cd ../Turbulence
 cp *.csv Lunarc_csv
+cp *.txt Lunarc_csv
 
 cd ../HVAC
 cp *.csv Lunarc_csv
+cp *.txt Lunarc_csv
 
 
--- Verification/Move_Lunarc_csv.sh
+++ Verification/Move_Lunarc_csv.sh
@@ -1,19 +1,25 @@
 cd Pressure_Solver
 cp Lunarc_csv/*.csv .
+cp Lunarc_csv/*.txt .
 
 cd ../Pressure_Effects
 cp Lunarc_csv/*.csv .
+cp Lunarc_csv/*.txt .
 
 cd ../Scalar_Analytical_Solution
 cp Lunarc_csv/*.csv .
+cp Lunarc_csv/*.txt .
 
 cd ../NS_Analytical_Solution
 cp Lunarc_csv/*.csv .
+cp Lunarc_csv/*.txt .
 
 cd ../Turbulence
 cp Lunarc_csv/*.csv .
+cp Lunarc_csv/*.txt .
 
 cd ../HVAC
 cp Lunarc_csv/*.csv .
+cp Lunarc_csv/*.txt .
 
 
