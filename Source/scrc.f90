a0527e044c83bab1ad3cd5a8ae593812b4470270
Susanne Kilian MacPro <s.kilian@hhpberlin.de>
2020-03-06 13:08:30 +0100

    add debug messages

--- Source/scrc.f90
+++ Source/scrc.f90
@@ -7,13 +7,8 @@
 !>  - WITH_SCARC_STANDALONE   : dump environment for ScaRC standalone version
 !> ================================================================================================================
 !#undef WITH_MKL
-<<<<<<< Updated upstream
 #define WITH_SCARC_VERBOSE
 #define WITH_SCARC_DEBUG
-=======
-#undef WITH_SCARC_VERBOSE
-#undef WITH_SCARC_DEBUG                                                                                                               
->>>>>>> Stashed changes
 #undef WITH_SCARC_MGM
 #undef WITH_SCARC_STANDALONE
 
