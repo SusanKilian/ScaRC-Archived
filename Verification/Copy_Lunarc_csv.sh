c073f592b6e5c87d4450faa130715d99525ff44f
Susanne Kilian <s.kilian@hhpberlin.de>
2020-06-29 10:53:44 +0200

    rework Lunarc transfer scripts

--- Verification/Copy_Lunarc_csv.sh
+++ Verification/Copy_Lunarc_csv.sh
@@ -1,25 +1,37 @@
 cd Pressure_Solver
+mkdir -p Lunarc_csv
 cp *.csv Lunarc_csv
+cp *.out Lunarc_csv
 cp *.txt Lunarc_csv
 
 cd ../Pressure_Effects
+mkdir -p Lunarc_csv
 cp *.csv Lunarc_csv
+cp *.out Lunarc_csv
 cp *.txt Lunarc_csv
 
 cd ../Scalar_Analytical_Solution
+mkdir -p Lunarc_csv
 cp *.csv Lunarc_csv
+cp *.out Lunarc_csv
 cp *.txt Lunarc_csv
 
 cd ../NS_Analytical_Solution
+mkdir -p Lunarc_csv
 cp *.csv Lunarc_csv
+cp *.out Lunarc_csv
 cp *.txt Lunarc_csv
 
 cd ../Turbulence
+mkdir -p Lunarc_csv
 cp *.csv Lunarc_csv
+cp *.out Lunarc_csv
 cp *.txt Lunarc_csv
 
 cd ../HVAC
+mkdir -p Lunarc_csv
 cp *.csv Lunarc_csv
+cp *.out Lunarc_csv
 cp *.txt Lunarc_csv
 
 
--- Verification/Move_Lunarc_csv.sh
+++ Verification/Move_Lunarc_csv.sh
@@ -1,25 +1,31 @@
 cd Pressure_Solver
 cp Lunarc_csv/*.csv .
+cp Lunarc_csv/*.out .
 cp Lunarc_csv/*.txt .
 
 cd ../Pressure_Effects
 cp Lunarc_csv/*.csv .
+cp Lunarc_csv/*.out .
 cp Lunarc_csv/*.txt .
 
 cd ../Scalar_Analytical_Solution
 cp Lunarc_csv/*.csv .
+cp Lunarc_csv/*.out .
 cp Lunarc_csv/*.txt .
 
 cd ../NS_Analytical_Solution
 cp Lunarc_csv/*.csv .
+cp Lunarc_csv/*.out .
 cp Lunarc_csv/*.txt .
 
 cd ../Turbulence
 cp Lunarc_csv/*.csv .
+cp Lunarc_csv/*.out .
 cp Lunarc_csv/*.txt .
 
 cd ../HVAC
 cp Lunarc_csv/*.csv .
+cp Lunarc_csv/*.out .
 cp Lunarc_csv/*.txt .
 
 
