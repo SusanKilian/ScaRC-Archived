5d75df3b834933672cbebaee5d530675e157609f
Susanne Kilian MacPro <s.kilian@hhpberlin.de>
2020-08-04 13:07:16 +0200

    remove unused

---
+++ Verification/Linear/2x1/do_dell.ps1
@@ -0,0 +1,32 @@
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe fft.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe glmat.fds
+.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_amg_fft_agg_l2_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_amg_fft_agg_l3_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_amg_fft_cub_l2_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_amg_fft_cub_l3_sm8.fds
+.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_amg_ssor_agg_l2_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_amg_ssor_agg_l3_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_amg_ssor_cub_l2_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_amg_ssor_cub_l3_sm8.fds
+.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cgamg_fft_agg_l2_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cgamg_fft_agg_l3_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cgamg_fft_cub_l2_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cgamg_fft_cub_l3_sm8.fds
+.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cgamg_ssor_agg_l2_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cgamg_ssor_agg_l3_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cgamg_ssor_cub_l2_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cgamg_ssor_cub_l3_sm8.fds
+.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cggmg_fft_l2_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cggmg_fft_l3_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cggmg_ssor_l2_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cggmg_ssor_l3_sm8.fds
+.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_gmg_fft_l2_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_gmg_fft_l3_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_gmg_ssor_l2_sm8.fds
+mpiexec -n 2 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_gmg_ssor_l3_sm8.fds
