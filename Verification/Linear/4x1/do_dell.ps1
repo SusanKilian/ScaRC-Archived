fe1b8560734f005f45c489ef642a73868acca072
Susanne Kilian hhpberlin <s.kilian@hhpberlin.de>
2020-08-04 17:32:26 +0200

    change number of MG iterations to 1 for cgamg

--- Verification/Linear/4x1/do_dell.ps1
+++ Verification/Linear/4x1/do_dell.ps1
@@ -1,11 +1,6 @@
 mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe fft.fds
 mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe glmat.fds
 
-mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cgamg_fft_gmg_l2_sm8.fds
-mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cgamg_ssor_gmg_l2_sm8.fds
-mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_amg_fft_gmg_l2_sm8.fds
-mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_amg_ssor_gmg_l2_sm8.fds
-
 mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_amg_fft_agg_l2_sm8.fds
 mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_amg_fft_agg_l3_sm8.fds
 mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_amg_fft_cub_l2_sm8.fds
@@ -35,3 +30,9 @@ mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_gmg_fft_
 mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_gmg_fft_l3_sm8.fds
 mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_gmg_ssor_l2_sm8.fds
 mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_gmg_ssor_l3_sm8.fds
+
+mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cgamg_fft_gmg_l2_sm8.fds
+mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_cgamg_ssor_gmg_l2_sm8.fds
+mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_amg_fft_gmg_l2_sm8.fds
+mpiexec -n 4 ../../../Build/impi_intel_win_64/fds_impi_win_64.exe scarc_amg_ssor_gmg_l2_sm8.fds
+
